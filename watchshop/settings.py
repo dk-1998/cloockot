49bf8ee1ba54a9b9f58d9bc626f5652af5f72625
dimitrije1998 <dimitrijek1998@gmail.com>
2026-05-05 12:46:21 -0700

    Izmene u settings.py

--- watchshop/settings.py
+++ watchshop/settings.py
@@ -26,7 +26,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
-DEFAULT_FROM_EMAIL = 'cloockot@gmail.com'
+DEFAULT_FROM_EMAIL = 'cloockot2026@gmail.com'
 EMAIL_TIMEOUT = 30
 
 # ======== Bezbednost ========

68f8565c358132f6e4e8946fb81297f6c4d77ca9
dimitrije1998 <dimitrijek1998@gmail.com>
2026-05-05 12:38:39 -0700

    Nove izmene u urls.py

--- watchshop/urls.py
+++ watchshop/urls.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from cloockot_watches.views import posalji_email  # uvezi funkciju
 urlpatterns = [
     path('admin/', admin.site.urls),
-  
     path('', include('cloockot_watches.urls'))
 ]
-
-
-
-

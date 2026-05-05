cbdec2fe222dfd5c786a3210eee76e05f43f0566
dimitrije1998 <dimitrijek1998@gmail.com>
2026-05-05 12:34:57 -0700

    Izmene u urls.py

--- watchshop/urls.py
+++ watchshop/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from cloockot_watches.views import posalji_email  # uvezi funkciju
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posalji-email/', posalji_email, name='posalji_email'),
+  
     path('', include('cloockot_watches.urls'))
 ]
 
